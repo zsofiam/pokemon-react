1e081b337ae165cb0f5d9dbbf40febdbeec22c70
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-08-27 11:17:14 +0200

    feat: use ThemeContext in App.js

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,36 +1,32 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from "react-router-dom";
 import {ThemeProvider} from 'styled-components';
 import H1 from '../../elements/H1';
 import Nav from '../../elements/Nav';
+import ThemeContext from '../../context/ThemeContext';
+import AppTheme from '../../Colors';
+
 
 function Header(){
+    
+const theme = useContext(ThemeContext);
+const currentTheme = AppTheme[theme];
+
     return (
-        <ThemeProvider theme={themeNormal}>
+        <ThemeProvider theme={currentTheme}>
         <Nav>
             <H1>Pokémon</H1>
+            <span>Choose Theme:</span>
+            <div></div>
+            <button style={{margin: '10px 10px 10px 0px'}}>BlackAndWhite</button>
+            <button style={{margin: ''}}>Fancy</button>
+            <div></div>
             <Link style={linkStyle} to="/pokemons">Pokemons</Link> |  
             <Link style={linkStyle} to="/types"> Types</Link>
         </Nav>
         </ThemeProvider>
     )
 }
-
-const themeFantasy = {
-    fontFamily: 'Fantasy',
-    fontSize: '3rem',
-    color: '#a88532',
-    background: '#a8327f',
-    backgroundHover: '#a8326d'
-};
-
-const themeNormal = {
-    fontFamily: 'sans-serif',
-    fontSize: '2em',
-    color: 'white',
-    background: 'black',
-    backgroundHover: '#444'
-};
 
 const linkStyle = {
     color: '#fff',
