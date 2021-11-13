1f3a1856618690e4d488fe62de9ddc86891fd276
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-11-13 19:17:34 +0100

    WIP: get background image with axios call

--- src/components/Pokemons.js
+++ src/components/Pokemons.js
@@ -3,16 +3,14 @@ import PokemonItem from "./PokemonItem";
 import PropTypes from "prop-types";
 
 const Pokemons = (props) => {
-
-     return props.pokemons.map((pokemon, index) => (
-        <PokemonItem key={index} id={index + 1} pokemon = {pokemon}/>
-    ));
-    
-}
+  return props.pokemons.map((pokemon, index) => (
+    <PokemonItem key={index} id={index + 1} pokemon={pokemon} />
+  ));
+};
 
 // PropTypes:
 Pokemons.propTypes = {
-    pokemons: PropTypes.array.isRequired,
-}
+  pokemons: PropTypes.array.isRequired,
+};
 
 export default Pokemons;
